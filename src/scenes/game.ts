c28d4197aea37718448a6ccba55683713ad8e345
Jim Löfgren <jim.lofgren@alster.se>
2023-11-09 21:26:45 +0100

    added stuff

--- src/scenes/game.ts
+++ src/scenes/game.ts
@@ -18,6 +18,12 @@ type Player = {
   dead?: boolean;
 };
 
+class Projectile extends Phaser.Physics.Arcade.Sprite {
+  constructor(scene: Phaser.Scene, position: { x: number; y: number }) {
+    super(scene, position.x, position.y, "bomb");
+  }
+}
+
 export default class GameScene extends Phaser.Scene {
   constructor() {
     super("game");
@@ -33,6 +39,7 @@ export default class GameScene extends Phaser.Scene {
   score: any = 0;
   scoreText: any = "";
   scoreBoard: any = "";
+  snowballs: any;
   bombs: any;
   gameOver: Boolean = false;
   left: any;
@@ -54,6 +61,20 @@ export default class GameScene extends Phaser.Scene {
         : ids.push(`you: ${player.id}`);
     });
     this.scoreBoard.setText(ids);
+  }
+
+  throwSnowball(x: number, y: number, direction?: string) {
+    const snowball = new Projectile(this, {
+      x,
+      y,
+    });
+    this.snowballs.add(snowball, true);
+    direction === "left"
+      ? snowball.setVelocityX(-500)
+      : snowball.setVelocityX(500);
+    this.physics.add.collider(this.snowballs, this.platforms, (snowball) => {
+      snowball.destroy();
+    });
   }
 
   preload() {
@@ -87,6 +108,10 @@ export default class GameScene extends Phaser.Scene {
     // see here https://labs.phaser.io/edit.html?src=src/camera/shake.js&v=3.60.0
     partySocket.addEventListener("message", (e) => {
       this.otherPlayers = JSON.parse(e.data) as Player[];
+    });
+
+    this.snowballs = this.physics.add.group({
+      collideWorldBounds: true,
     });
 
     // game scene and platsforms
@@ -207,6 +232,17 @@ export default class GameScene extends Phaser.Scene {
 
     // implementing player moves functionality
     this.cursors = this.input.keyboard.createCursorKeys();
+    var spaceBar = this.input.keyboard.addKey(
+      Phaser.Input.Keyboard.KeyCodes.SPACE
+    );
+
+    spaceBar.on("down", () => {
+      this.throwSnowball(
+        this.player.x,
+        this.player.y,
+        this.player.anims.currentAnim.key
+      );
+    });
 
     this.gifts = this.physics.add.group({
       key: "gift",
@@ -319,6 +355,7 @@ export default class GameScene extends Phaser.Scene {
             //newSprite.anims.play(this.otherPlayers[i].direction, true);
             this.otherSprites.push(newSprite);
             this.physics.add.collider(this.player, newSprite);
+            this.physics.add.collider(this.snowballs, newSprite);
           }
         }
       }
